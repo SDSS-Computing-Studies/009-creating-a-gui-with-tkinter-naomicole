d9842615eda756c5ec18d0007344a32ad7727e15
naomicole <71406713+naomicole@users.noreply.github.com>
2020-11-02 10:47:12 -0800

    .

--- task3.py
+++ task3.py
@@ -1,1 +1,11 @@
 #!python3
+
+import tkinter as tk 
+from tkinter import *
+from tkinter import ttk
+
+window = tk.Tk()
+window.title("Example")
+
+dogphoto = PhotoImage(file="dog.png")
+labeld = tk.Label(window,image=dogphoto)
