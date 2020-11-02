97c8137ee8b479474c8352e6d4ec88eb753110b0
naomicole <71406713+naomicole@users.noreply.github.com>
2020-11-02 11:13:56 -0800

    .

--- task3.py
+++ task3.py
@@ -9,3 +9,11 @@ window.title("Example")
 
 dogphoto = PhotoImage(file="dog.png")
 labeld = tk.Label(window,image=dogphoto)
+label1 = tk.Label(window,text="Pochacco!")
+label2 = tk.Label(window, text="A cuddly little puppy! This is from the same\n creators who brought you Keropi and Kero Kero",bg="CadetBlue1")
+
+labeld.grid(row = 1, column = 2, rowspan = 3)
+label1.grid(row = 2, column = 3)
+label2.grid(row = 4, column = 1, columnspan = 4)
+
+window.mainloop()
